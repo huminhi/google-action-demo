eb017292cc245078f9f573492e0a707ea4fc4d94
hieuhuynh <huminhi@gmail.com>
2018-03-02 20:17:56 +0700

    package

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.actionDemo = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+})
